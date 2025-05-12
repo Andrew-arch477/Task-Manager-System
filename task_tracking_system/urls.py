a6b09660b369589480edd873ceba199931765db2
Andrew <forandrew1985@gmail.com>
2025-05-12 09:55:42 +0200

    Adding_task_view_for_showing_tasks_also_creating_and_editing_urls_and_creating_tasks_main_page

---
+++ task_tracking_system/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('Task_main/', views.Task_main.as_view(), name='Task_main_page'),
+]
--- task_tracking_system/views.py
+++ task_tracking_system/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+from django.views.generic.base import TemplateView
+from task_tracking_system.models import Task, Comment
 
-# Create your views here.
+class Task_main(FormView):
+    template_name = 'Task_main.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tasks"] = Task.objects.all()
+        return context
+
