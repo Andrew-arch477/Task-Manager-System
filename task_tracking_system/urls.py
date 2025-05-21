567b6aaa9a717b2dce207a2be8d900cdc34b4560
Andrew-arch477 <forandrew1985@gmail.com>
2025-05-21 19:24:17 +0300

    Visual upgrades.

--- task_tracking_system/urls.py
+++ task_tracking_system/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('Task_main/', views.Task_main.as_view(), name='Task_main_page'),
+    path('Task_main/', views.Task_ViewPage.as_view(), name='Task_main_page'),
     path('Task_create/', views.Task_Create.as_view(), name='Task_create_page'),
 ]
